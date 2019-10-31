53882c41eea36facc5e0903035b97570fd0a3155
이재호 <uss425@naver.com>
2019-10-31 14:34:56 +0900

    주석 추가

--- SSU_Chess/SSU_Chess/Main.c
+++ SSU_Chess/SSU_Chess/Main.c
@@ -7,9 +7,9 @@ enum color
 	Lcyan, Lred, Lmagenta, Yellow, White
 };
 
-void SetColor(WORD text, WORD back);
-void GotoXY(int y, int x);
-void InitBoard();
+void SetColor(WORD text, WORD back); //text와 background의 색상을 변경한다.
+void GotoXY(int y, int x);			 //gotoxy 함수
+void InitBoard();					 //체스보드를 최초 출력한다.
 
 
 int main()
@@ -19,38 +19,39 @@ int main()
 	return 0;
 }
 
-void SetColor(WORD text, WORD back)
+void SetColor(WORD text, WORD back) //text와 background의 색상을 변경한다.
 {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), text + back * 16);
 }
 
-void GotoXY(int y, int x)
+void GotoXY(int y, int x) //gotoxy 함수
 {
 	COORD pos = { x,y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
-void InitBoard()
+void InitBoard() //체스보드를 최초 출력한다.
 {
-	int flag = -1;
+	int flag = -1; //흑과 백의 색깔 스위칭 변수 -1을 곱하면서 스위칭.
 
-	for (int y = 0; y < 8; y++)
+	for (int y = 0; y < 8; y++) 
 	{
 		for (int x = 0; x < 8; x++)
 		{
+			// flag값에 따라 색상을 변경한다.
 			if (flag == -1) SetColor(0, White);
-			else SetColor(0, Black);
+			else if (flag == 1) SetColor(0, Black);
 
+			/*====(y, x)칸 출력부=========*/
 			for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
 			{
-				for (int j = 0; j < 3; j++)
-				{
-					GotoXY(3 * y + i, 6 * x + 2 * j);
-					printf("　");
-				}
+				GotoXY(3 * y + i, 6 * x + 2 * j);
+				printf("　");
 			}
-			flag *= -1;
+			/*===========================*/
+			flag *= -1; //한칸 출력이 끝나면, 색상을 스위칭한다.
 		}
-		flag *= -1;
+		flag *= -1; //한줄 출력이 끝나면, 색상을 스위칭한다.
 	}
 }
