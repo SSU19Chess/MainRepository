7e2a54f47ab87effe11d3631c55bdfca08cb95cf
Ladun <ladun9128@gmail.com>
2019-11-04 13:34:38 +0900

    GameData Add

--- SSU_Chess/SSU_Chess/GameData.h
+++ SSU_Chess/SSU_Chess/GameData.h
@@ -1,1 +1,40 @@
 #pragma once
+#pragma once
+
+// Inputs
+#define LEFT  75
+#define RIGHT  77
+#define UP   72
+#define DOWN 80
+#define ENTER  13
+
+//
+#define WHITE_PLAYER 0
+#define BLACK_PLAYER 1
+
+//
+#define CHESS_RANK 8
+#define CHESS_FILE 8
+
+typedef enum _COLOR { Black = 0, Blue, Green, Cyan, Red, Magenta, Brown, Lgray, Dgray, Lblue, Lgreen, Lcyan, Lred, Lmagenta, Yellow, White } COLOR;
+
+typedef enum _PIECETYPE { NONE, PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING } PIECETYPE;
+
+typedef struct _POS
+{
+	int x, y;
+}POS;
+
+typedef struct _STATEDATA
+{
+	PIECETYPE picecType;
+	int player;
+}STATEDATA;
+
+typedef struct _CHESS
+{
+	STATEDATA states[CHESS_RANK][CHESS_FILE];
+
+	int queenSideCastling[2];
+	int kingSideCastling[2];
+}CHESS;
